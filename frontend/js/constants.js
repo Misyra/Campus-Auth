533a636f8966153b79915fa88b4c4642d5c5300f
Misyra <Misyra@163.com>
2026-06-13 18:22:55 +0800

    fix: 删除 constants.js 中的 use_global_advanced

--- frontend/js/constants.js
+++ frontend/js/constants.js
@@ -177,10 +177,8 @@ export const DEFAULT_PROFILE_SETTINGS = {
   username: '',
   password: '',
   use_global_credentials: true,
-  use_global_advanced: true,
   use_global_auth_url: true,
   use_global_task: true,
   auth_url: '',
   active_task: '',
-  browser_user_agent: '',
 };
